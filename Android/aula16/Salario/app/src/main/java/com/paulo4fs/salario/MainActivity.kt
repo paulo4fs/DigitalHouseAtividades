a45ced97415cf8a1f6cb5c106e74c5a23e15413e
paulo4fs <engpaulofrota@gmail.com>
2020-10-03 11:53:43 -0300

    App calculadora salario criado

---
+++ Android/aula16/Salario/app/src/main/java/com/paulo4fs/salario/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.paulo4fs.salario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnCalcular.setOnClickListener {
+            calcularSalario(
+                horasTrabalhadas.text.toString(),
+                valorDaHora.text.toString()
+            )
+        }
+    }
+
+    fun calcularSalario(horas: String, valor: String) {
+        if (horas.isNullOrBlank() || valor.isNullOrBlank()) {
+            txtSalario.text = "R$ XXXXX,XX"
+        } else if (horas.toDouble() > 0 && valor.toDouble() > 0) {
+            val salario = horas.toDouble() * valor.toDouble()
+            txtSalario.text = "R$ $salario"
+        }
+    }
+}
