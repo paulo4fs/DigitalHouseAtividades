0b7f277366f1cbd1acbb53cbd1dedd90817b2ae2
paulo4fs <engpaulofrota@gmail.com>
2020-10-03 10:30:57 -0300

    App par ou impar criado

--- Android/aula16/ParOuImpar/app/src/main/java/com/paulo4fs/parouimpar/MainActivity.kt
+++ Android/aula16/ParOuImpar/app/src/main/java/com/paulo4fs/parouimpar/MainActivity.kt
@@ -2,10 +2,17 @@ package com.paulo4fs.parouimpar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnConverter.setOnClickListener { converter(temperaturaC.text.toString()) }
+    }
+
+    fun converter(tempC: String) {
+        val tempF = ((tempC.toDouble() * 9) / 5) + 32
+        temperaturaF.text = "$tempF ºF"
     }
 }
