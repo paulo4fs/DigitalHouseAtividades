ff5326b318826ffd2f8967de83bfbe88e3b64e3c
paulo4fs <engpaulofrota@gmail.com>
2020-10-17 19:33:09 -0300

    aula 21 bonus

--- Android/aula21/Aula21bonus/app/src/main/java/com/paulo4fs/aula21bonus/AnoFragment.kt
+++ Android/aula21/Aula21bonus/app/src/main/java/com/paulo4fs/aula21bonus/AnoFragment.kt
@@ -1,15 +1,14 @@
 package com.paulo4fs.aula21bonus
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_ano.*
+import kotlinx.android.synthetic.main.fragment_ano.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,13 +16,25 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class AnoFragment : Fragment() {
+    lateinit var iClicou: IClicou
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ano, container, false)
+        val view = inflater.inflate(R.layout.fragment_ano, container, false)
+
+        view.btn.setOnClickListener {
+            var nomeText: String = view.textInputFieldNome.text.toString()
+            val anoText: String = view.textInputFieldAno.text.toString()
+            iClicou.calcularIdade(nomeText, anoText.toInt())
+        }
+        return view
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        iClicou = context as IClicou
+    }
 }
--- Android/aula21/Aula21bonus/app/src/main/java/com/paulo4fs/aula21bonus/MainActivity.kt
+++ Android/aula21/Aula21bonus/app/src/main/java/com/paulo4fs/aula21bonus/MainActivity.kt
@@ -2,15 +2,29 @@ package com.paulo4fs.aula21bonus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_result.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IClicou {
+    val resultFragment = ResultFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var fragAno : AnoFragment? = null
-        var fragResult : ResultFragment? = null
+        val manager = supportFragmentManager
 
+        val transaction = manager.beginTransaction()
+        transaction.add(R.id.anoFragment, AnoFragment())
+        transaction.commit()
 
+        val transaction2 = manager.beginTransaction()
+        transaction2.replace(R.id.resultFragment, resultFragment)
+        transaction2.commit()
+    }
+
+    override fun calcularIdade(nome: String, ano: Int) {
+        val anoAtual = Calendar.getInstance().get(Calendar.YEAR)
+        resultFragment.mostrarResultado(nome, anoAtual - ano)
     }
 }
